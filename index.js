f543aa83ada4d50268c18afc1ff7939c370b5b98
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-17 12:07:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,11 @@
 function writeCards(arr , even) {
   let thanks = [];
   for(let i = 0 ; i < arr.length ; i++){
+<<<<<<< HEAD
     thanks[i]=`Thank you, ${arr[i]}, for the wonderful ${even} gift!`;
+=======
+    thanks[i]=`thank you, ${arr[i]}, for the wonderful ${even} gift!`;
+>>>>>>> 62b9c96edff355b846200b6f39e0fe696e58022d
   }
 
 
